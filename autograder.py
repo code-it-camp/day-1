582c3a368f29f98a9e7f54b1e88974752996de97
code_IT <code.it.camp@gmail.com>
2017-06-09 06:39:44 -0700

    last commit for night

--- autograder.py
+++ autograder.py
@@ -1,25 +1,22 @@
 import sys
 import unittest
 from graderlib import test_case
+import practice as mod
 
-import practice
+def run_test(questions):
+	for question in questions:
+		# test is the instance of our TestCase class with all of our
+		# desired question, specified by argv[1], functions to be tested.
+		test = test_case(mod, question, question_inputs)
 
-def run_test(module, question):
-	# test is the instance of our TestCase class with all of our
-	# desired question, specified by argv[1], functions to be tested.
-	test = test_case(module, question, question_inputs)
+		# not test corresponds to a requested question test which
+		# doesn't exist
+		if not test:
+			return
 
-	# not test corresponds to a requested question test which
-	# doesn't exist
-	if not test:
-		return
-
-	# adds our TestCase to the global attributes so that unittest
-	# will find it when it searches for TestCases to run
-	globals()[test.__name__] = test
-
-	print()
-	print('Question', question[1:])
+		# adds our TestCase to the global attributes so that unittest
+		# will find it when it searches for TestCases to run
+		globals()[test.__name__] = test
 
 	"""
 	argv='q' allows the program to use the command line arguments
@@ -31,20 +28,18 @@ def run_test(module, question):
 	function tries to split the argv value into single characters so
 	unless we have each of those individual characters defined as
 	dummy attribute classes of __main__ (which there's no reason to
-	do so), just leave argv as 'q' be and accept the ludicracy of
-	this frickin stupid argument.
+	do so), so leave argv='q'.
 	"""
 	unittest.main(argv='q')
 
-def main(module):
+def main():
 	# Test all questions.
 	if len(sys.argv) == 1:
-		for question in question_inputs:
-			run_test(module, question)
+		run_test([question for question in question_inputs])
 	# Test one specific question specified
 	# by the single argument passed in.
 	elif len(sys.argv) == 2:
-		run_test(module, sys.argv[1])
+		run_test([sys.argv[1]])
 	# Too many arguments.
 	else:
 		print("Too many arguments. Check the README.md "
@@ -76,8 +71,92 @@ question_inputs = {
 			[(56, 8), 7],
 			[(84, 7), 12]
 		]
+	},
+	"q2": {
+		"quart": [
+			[(2), 16],
+			[(3), 81],
+			[(4), 256]
+		],
+		"cube": [
+			[(2), 8],
+			[(5), 125],
+			[(19), 6859]
+		]
+	},
+	"q3": {
+		"max_val1": [
+			[(1, 2), 2],
+			[(342342, 812981), 812981],
+			[(-812, -811), -811]
+		],
+		"min_val1": [
+			[(31, 6), 6],
+			[(1323,122), 122],
+			[(65,-73), -73]
+		],
+		"max_val2": [
+			[(281, 120, 321, 6645), 6445],
+			[(62344, 334, 52346, 333), 62344],
+			[(294, 21, 94, -818), 294]
+		],
+		"min_val2": [
+			[(1, 2, 3, 4, 5, 6),],
+			[(-1, 523, 292, 2108, 30, -13,), -13],
+			[(6, 8, 86, 61, 42, 50), 6]
+		],
+	},
+	"q4": {
+		"merge_strings": [
+			[("a", "b"), "ab"],
+			[("cat", "dog"), "catdog"],
+			[("meat", "ball"), "meatball"],
+			[([1, 40, 3], [2, 4, 6]), [1, 40, 3, 2, 4, 6]]
+		],
+		"stringed_out": [
+			[("abcde"), ['a', 'c', 'e']],
+			[("aabbcc"), ['a', 'b', 'c']],
+			[("1ab23c"), ['1', 'b', '3']],
+			[([0, 1, 2, 3, 4]), [0, 2, 4]]
+		],
+		"length_of_str": [
+			[('abcdefghijklm'), 12],
+			[('code it!'), 8],
+			[([3, 1, 2, 0]), 4]
+		],
+		"slice1": [
+			[('yeah yeah yeah', 9), 'yeah'],
+			[('The Beatles', 5), 'Beatles'],
+			[('lampost', 3), 'post'],
+			[(['1', 2, '3', 4, '5', 6], 3), [4, '5', 6]]
+		],
+		"slice2": [
+			[('heyo', 3), 'hey'],
+			[('suppppppp', 5), 'suppp'],
+			[('worst trade deal ever', 3), 'wo'],
+			[(['hi', 'hello', 'howdy', 'cheerio'], 4), ['hi', 'hello', 'howdy']]
+		],
+		"slice_and_dice": [
+			[('abc123zyx987', 4, 8), '23zy'],
+			[('racecar', 1, 6), 'aceca'],
+			[("(^::'^(", 0, 3), '(^:'],
+			[([1, 2, 3, 4, 5], 3, 4), [4]]
+		],
+		"chop_it_up": [
+			[('Rolltide', 'iiiiinnn'), 'Rollinnn'],
+			[('yummy', 'tummy'), 'yummy'],
+			[('Joe Biden ', 'Obama'), 'Joe Bama'],
+			[([1, 2, 3, 4], [9, 8, 6, 6]), [1, 2, 9, 8]]
+		]
+	},
+	"q5": {
+		"powerful_func": [
+			[([1, 2, 3, -1, 4, 2]), 1],
+			[(4, 2, 1, 0.5, 0.25, 0.125), 0.0625],
+			[(3, 5, 7, 1, 3, 5), 9]
+		]
 	}
 }
 
 if __name__ == '__main__':
-	main(practice)
+	main()
--- practice.py
+++ practice.py
@@ -1,4 +1,4 @@
-#Question 1: Math Operations
+# Question 1: Math Operations
 # ---------------------------------------------------------- #
 def add(a, b):
 	"""
@@ -11,8 +11,8 @@ def add(a, b):
 	Returns:
 		the sum of a and b
 	"""
-	#FIX ME
-	return a + b
+	# FIXME
+	return 
 
 def sub(a, b):
 	"""
@@ -25,8 +25,8 @@ def sub(a, b):
 	Returns:
 		the differnce of b from a
 	"""
-	#FIX ME
-	return a - b
+	# FIXME
+	return 
 
 def mul(a, b):
 	"""
@@ -39,8 +39,8 @@ def mul(a, b):
 	Returns:
 		the product of a and b
 	"""
-	#FIX ME
-	return a * b
+	# FIXME
+	return 
 
 def div(a, b):
 	"""
@@ -53,12 +53,275 @@ def div(a, b):
 	Returns:
 		the quotient of a by b
 	"""
-	#FIX ME
-	return a / b
+	# FIXME
+	return 
+
+def sqr(x):
+	"""
+	Computes the square of x.
+
+	Args:
+		x: number to be squared
+
+	Returns:
+		the square of x
+	"""
+	# FIXME
+	return 
+
 # ---------------------------------------------------------- #
 
-#Question 2
+# Question 2
 # ---------------------------------------------------------- #
+def quart(x):
+	"""
+	Computes the fourth power of x without explicitly writing
+	x * x * x * x or sqr(x) * sqr(x). HINT: use a previously
+	defined function
+
+	Args:
+		x: number to quarted
+
+	Returns:
+		the fourth power of x
+	"""
+	# FIXME
+	return 
+
+def cube(x):
+	"""
+	Computes the third power of x without explicitly writing
+	x * x * x. HINT: use a previously defined function
+
+	Args:
+		x: number to quarted
+
+	Returns:
+		the third power of x
+	"""
+	# FIXME
+	return 
+
+# ---------------------------------------------------------- #
+
+# Question 3
+# ---------------------------------------------------------- #
+def max_val1(x1, x2):
+	"""
+	Determines the larger of the two arguments passed in.
+
+	Args:
+		x1: first number in comparison
+		x2: second number in comparison
+
+	Returns:
+		the larger of the two numbers
+	"""
+	# FIXME
+	return 
+
+def min_val1(x1, x2):
+	"""
+	Determines the smaller of the two arguments passed in.
+
+	Args:
+		x1: first number in comparison
+		x2: second number in comparison
+
+	Returns:
+		the smaller of the two numbers
+	"""
+	# FIXME
+	return 
+
+def max_val2(x1, x2, x3, x4):
+	"""
+	Determines the largest of the four arguments passed in.
+
+	Args:
+		x1: first number in comparison
+		x2: second number in comparison
+		x3: third number in comparison
+		x4: fourth number in comparison
+
+	Returns:
+		the largest of the four numbers
+	"""
+	# FIXME
+	return 
+
+def min_val2(x1, x2, x3, x4, x5, x6):
+	"""
+	Determines the smallest of the six arguments passed in.
+
+	Args:
+		x1: first number in comparison
+		x2: second number in comparison
+		x3: third number in comparison
+		x4: fourth number in comparison
+		x5: fifth number in comparison
+		x6: sixth number in comparison
+
+	Returns:
+		the smallest of the six numbers
+	"""
+	# FIXME
+	return 
+
+# ---------------------------------------------------------- #
+
+# Question 4
+# ---------------------------------------------------------- #
+
+def merge_strings(str1, str2):
+	"""
+	Makes a new string composed of merging
+	the two strings together. HINT: think of
+	how to add two strings together
+
+	Args:
+		str1: first part of the merged string
+		str2: second part of the merged string
+
+	Returns:
+		new string which is combo of str1 and str2
+	"""
+	# FIXME
+	return 
+
+def stringed_out(str0):
+	"""
+	Makes a list of the first, third, and fifth
+	characters in the provided string. HINT: strings
+	can be indexed too. Assume the provided string
+	will always be at least 5 characters long.
+
+	Args:
+		str0: string to be "stringed out"
+
+	Returns:
+		list of 1st, 3rd and 5th letters of str0
+	"""
+	# FIXME
+	return 
+
+def length_of_str(str0):
+	"""
+	Determines the length of a string.
+	HINT: There is a built-in function
+	that will help you out a lot here.
+
+	Args:
+		str0: string
+
+	Returns:
+		the length of the string
+	"""
+	# FIXME
+	return 
+
+def slice1(str0, start):
+	"""
+	Makes a string which is just str0 started
+	from the character at the start index.
+
+	Args:
+		str0: string to slice
+		start: number, where to start the new string
+			from str0
+
+	Returns:
+		a string which is all the characters of str0
+		after and including the start index
+	"""
+	# FIXME
+	return 
+
+def slice2(str0, end):
+	"""
+	Makes a string which is just str0 from the
+	beginning character up to the but not including
+	the end index.
+
+	Args:
+		str0: string to slice
+		end: number, where to end the new string
+			from str0
+
+	Returns:
+		a string which is all the characters of str0
+		before and excluding the end index
+	"""
+	# FIXME
+	return 
+
+def slice_and_dice(str0, start, end):
+	"""
+	Makes a string which is str0 started
+	from the character at the start index and ended
+	at the end index. NO PRIMITIVE OPERATIONS allowed
+	in this function i.e. no touching str0 directly
+	inside slice_and_dice.
+
+	Args:
+		str0: string to slice
+		start: number, where to start the new string
+			from str0
+		end: number, wher to end the new string
+			from str0
+
+	Returns:
+		a string which is all the characters of str0
+		from the start index up to the end index
+	"""
+	# FIXME
+	return 
+
+def chop_it_up(str0, str1):
+	"""
+	Make a new string which consists of the first
+	half of str0 combined with a space in between
+	the second half of str1. HINT: a function from
+	a previous question will come in handy here.
+
+	Args:
+		str0: former part of new string
+		str1: latter part of new string
+
+	Returns:
+		a new string which is the combination of
+		str0 and str1
+	"""
+	# FIXME
+	return 
+
+# ---------------------------------------------------------- #
+
+# Question 5
+# ---------------------------------------------------------- #
+def powerful_func(lst):
+	"""
+	Determines the minimum number from the
+	1st, 3rd and 5th values of the passed
+	in list as well as the minimum number
+	from the 2nd, 4th and 6th values of the
+	passed in list. Then determines the
+	maximum of these two values and squares it.
+
+	Args:
+		lst: a list containing at least 6 numbers
+
+	Returns:
+		a tuple containing first the min of the 1, 3,
+		5 numbers in lst, second the min of the 2, 4,
+		6 numbers in lst, third the square of the
+		max of these two numbers
+	"""
+	# FIXME
+	first = 1
+	second = 2
+	third = 3
+	return (first, second, third)
 
 if __name__ == '__main__':
 	main()
